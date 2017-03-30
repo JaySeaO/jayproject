3de2aae291a57b95c0cc864e9650eb08825f5085
JaySeaO <jcomarmond@gmail.com>
2017-03-30 22:03:09 +0200

    Add detail view for character specific fights

--- sfv/views.py
+++ sfv/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the SFV index.")
+
+def detail(request, character_id):
+    return HttpResponse("You're looking at character %s.", character_id)
+
