03aec9658d77845e4a2cdf7fac61b05d1f8fa8bf
JaySeaO <jcomarmond@gmail.com>
2017-03-30 22:20:23 +0200

    add a somewhat real view

--- sfv/views.py
+++ sfv/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Fight
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the SFV index.")
+    latest_fight_list =  Fight.objects.order_by('-fight_date')[:10]
+    template = loader.get_template('sfv/index.html')
+    context = {
+        'latest_fight_list': latest_fight_list
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def detail(request, character_id):
-    return HttpResponse("You're looking at character %s.", character_id)
+    return HttpResponse("You're looking at character %s." % character_id)
 
