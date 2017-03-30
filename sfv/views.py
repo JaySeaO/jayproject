258db74e0e2e962a4db83e4688f668572a12739b
JaySeaO <jcomarmond@gmail.com>
2017-03-30 22:28:13 +0200

    View writter smartlier... with more smartness...

--- sfv/views.py
+++ sfv/views.py
@@ -1,16 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Fight
 
 def index(request):
     latest_fight_list =  Fight.objects.order_by('-fight_date')[:10]
-    template = loader.get_template('sfv/index.html')
     context = {
         'latest_fight_list': latest_fight_list
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'sfv/index.html', context)
 
 def detail(request, character_id):
     return HttpResponse("You're looking at character %s." % character_id)
