2ff0de15002837b2b43b6ac285403112085ef3cf
JaySeaO <jcomarmond@gmail.com>
2017-04-03 20:59:38 +0200

    missing import

--- sfv/views.py
+++ sfv/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Character, Fight
 
